125c6b698e0099b5f91224227ff67937a3045605
Karl Scherzer <scherzer1999@hotmail.com>
2022-05-08 23:21:56 -0500

    Poniendole diseño a los botones de productos de administrador

--- src/pages/Administrador/Productos/Productos.js
+++ src/pages/Administrador/Productos/Productos.js
@@ -13,191 +13,189 @@ import { useForm } from "react-hook-form";
 import { FormState } from "react-hook-form";
 import { ToastContainer, toast } from "react-toastify";
 import { Flip } from "react-toastify";
+import "./styles.css";
 
 //import PubSub from "pubsub-js";
 
 const correcto = (mensaje) => {
-  toast.success(mensaje, {
-    position: "bottom-left",
-    autoClose: 1000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: false,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-    transition: Flip,
-  });
+	toast.success(mensaje, {
+		position: "bottom-left",
+		autoClose: 1000,
+		hideProgressBar: false,
+		closeOnClick: true,
+		pauseOnHover: false,
+		draggable: true,
+		progress: undefined,
+		theme: "colored",
+		transition: Flip,
+	});
 };
 const error = (mensaje) => {
-  toast.error(mensaje, {
-    position: "bottom-left",
-    autoClose: 1000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: false,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-    transition: Flip,
-  });
+	toast.error(mensaje, {
+		position: "bottom-left",
+		autoClose: 1000,
+		hideProgressBar: false,
+		closeOnClick: true,
+		pauseOnHover: false,
+		draggable: true,
+		progress: undefined,
+		theme: "colored",
+		transition: Flip,
+	});
 };
 
 const CAMPOS = [
-  "ID",
-  "Producto",
-  "Cantidad",
-  "Precio",
-  "Modificar",
-  "Eliminar",
-  "Detalles",
+	"ID",
+	"Producto",
+	"Cantidad",
+	"Precio",
+	"Modificar",
+	"Eliminar",
+	"Detalles",
 ];
 
 const eliminar = (e) => {
-  delProductos(e.target.className).then((response) => {
-    if (response) {
-      alert("Producto El producto se elimino");
-      correcto("El producto se elimino");
-    } else {
-      error("Ocurrió un error al eliminar");
-    }
-  });
+	delProductos(e.target.className).then((response) => {
+		if (response) {
+			correcto("El producto se elimino");
+		} else {
+			error("Ocurrió un error al eliminar");
+		}
+	});
 };
 
-const detalles = (e) => alert(e.target.className);
-
 const Productos = () => {
-  const [modalProducto, setModalProducto] = useState(false);
-  const [productos, setProductos] = useState([]);
-  const [search, setSearch] = useState("");
-  const [addProducto, setAddProducto] = useState(false);
+	const [modalProducto, setModalProducto] = useState(false);
+	const [productos, setProductos] = useState([]);
+	const [search, setSearch] = useState("");
+	const [addProducto, setAddProducto] = useState(false);
 
-  const [idProducto, setIdProducto] = useState(0);
-  const [poderosoJson, setPoderosoJson] = useState({});
+	const [idProducto, setIdProducto] = useState(0);
+	const [poderosoJson, setPoderosoJson] = useState({});
 
-  //Modificar producto
-  const [modalModificarProducto, setModalModificarProducto] = useState(false);
-  const [modalDetallesProducto, setModalDetallesProducto] = useState(false);
+	//Modificar producto
+	const [modalModificarProducto, setModalModificarProducto] = useState(false);
+	const [modalDetallesProducto, setModalDetallesProducto] = useState(false);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm();
 
-  //Funcion para modal de modificar
-  const handleUpdate = (e) => {
-    //const id = e.target.className;
+	//Funcion para modal de modificar
+	const handleUpdate = (e) => {
+		//const id = e.target.className;
 
-    const find = productos.find(
-      (producto) => producto.id_PR === e.target.className
-    );
+		const find = productos.find(
+			(producto) => producto.id_PR === e.target.className
+		);
 
-    setIdProducto(e.target.className);
+		setIdProducto(e.target.className);
 
-    reset({
-      nombre: find.nombre_PR,
-      precio_inversion: find.inversion_PR,
-      cantidad: find.cantidad_PR,
-      precio_publico: find.precio_PR,
-      unidadPeso_PR: find.unidadPeso_PR,
-      piezasCaja_PR: find.piezasCaja_PR,
-      descripcion_pro: find.descripcion_PR,
-    });
+		reset({
+			nombre: find.nombre_PR,
+			precio_inversion: find.inversion_PR,
+			cantidad: find.cantidad_PR,
+			precio_publico: find.precio_PR,
+			unidadPeso_PR: find.unidadPeso_PR,
+			piezasCaja_PR: find.piezasCaja_PR,
+			descripcion_pro: find.descripcion_PR,
+		});
 
-    setModalModificarProducto(!modalModificarProducto);
-    setPoderosoJson(Object.values(find));
+		setModalModificarProducto(!modalModificarProducto);
+		setPoderosoJson(Object.values(find));
 
-    // PubSub.publish("saludo", Object.values(find));
-  };
+		// PubSub.publish("saludo", Object.values(find));
+	};
 
-  const handleAdd = () => {
-    setModalProducto(!modalProducto);
-  };
+	const handleAdd = () => {
+		setModalProducto(!modalProducto);
+	};
 
-  const handleDetal = (e) => {
-    const find = productos.find(
-      (producto) => producto.id_PR === e.target.className
-    );
+	const handleDetal = (e) => {
+		const find = productos.find(
+			(producto) => producto.id_PR === e.target.className
+		);
 
-    reset({
-      nombre: find.nombre_PR,
-      precio_inversion: find.inversion_PR,
-      cantidad: find.cantidad_PR,
-      precio_publico: find.precio_PR,
-      unidadPeso_PR: find.unidadPeso_PR,
-      piezasCaja_PR: find.piezasCaja_PR,
-      descripcion_pro: find.descripcion_PR,
-      foto_PR: find.foto_PR,
-    });
+		reset({
+			nombre: find.nombre_PR,
+			precio_inversion: find.inversion_PR,
+			cantidad: find.cantidad_PR,
+			precio_publico: find.precio_PR,
+			unidadPeso_PR: find.unidadPeso_PR,
+			piezasCaja_PR: find.piezasCaja_PR,
+			descripcion_pro: find.descripcion_PR,
+			foto_PR: find.foto_PR,
+		});
 
-    setIdProducto(e.target.className);
-    setModalDetallesProducto(!modalModificarProducto);
-    setPoderosoJson(Object.values(find));
-  };
+		setIdProducto(e.target.className);
+		setModalDetallesProducto(!modalModificarProducto);
+		setPoderosoJson(Object.values(find));
+	};
 
-  useEffect(() => {
-    getProductosInventario().then((response) => setProductos(response));
-  }, [productos]);
+	useEffect(() => {
+		getProductosInventario().then((response) => setProductos(response));
+	}, [productos]);
 
-  const nuevas_propiedades = productos.map((producto) => ({
-    id_PR: producto.id_PR,
-    nombre_PR: producto.nombre_PR,
-    cantidad_PR: producto.cantidad_PR,
-    precio_PR: producto.precio_PR,
-  }));
+	const nuevas_propiedades = productos.map((producto) => ({
+		id_PR: producto.id_PR,
+		nombre_PR: producto.nombre_PR,
+		cantidad_PR: producto.cantidad_PR,
+		precio_PR: producto.precio_PR,
+	}));
 
-  return (
-    <>
-      <AdminBuscador keyword={setSearch} />
-      <Table
-        campos={CAMPOS}
-        datos={Object.values(nuevas_propiedades)}
-        filtro="nombre_PR"
-        search={search}
-        tipo={2}
-        funciones={[handleUpdate, eliminar, handleDetal]} //modificar, eliminar, detalles
-      />
+	return (
+		<>
+			<AdminBuscador keyword={setSearch} />
+			<Table
+				campos={CAMPOS}
+				datos={Object.values(nuevas_propiedades)}
+				filtro="nombre_PR"
+				search={search}
+				tipo={2}
+				funciones={[handleUpdate, eliminar, handleDetal]} //modificar, eliminar, detalles
+			/>
 
-      <div className="contenedor-button">
-        <Button onClick={handleAdd}>Agregar producto</Button>
-      </div>
+			<div className="contenedor-button">
+				<Button onClick={handleAdd}>Agregar producto</Button>
+			</div>
 
-      <RegistrarProducto
-        modalProducto={modalProducto}
-        setModalProducto={setModalProducto}
-        addProducto={addProducto}
-        setAddProducto={setAddProducto}
-      />
+			<RegistrarProducto
+				modalProducto={modalProducto}
+				setModalProducto={setModalProducto}
+				addProducto={addProducto}
+				setAddProducto={setAddProducto}
+			/>
 
-      <ModificarProducto
-        modalModificarProducto={modalModificarProducto}
-        setModalModificarProducto={setModalModificarProducto}
-        addProducto={addProducto}
-        setAddProducto={setAddProducto}
-        idProducto={idProducto}
-        poderosoJson={poderosoJson}
-        register={register}
-        handleSubmit={handleSubmit}
-        errors={errors}
-        reset
-      />
-      <DetallesProducto
-        modalDetallesProducto={modalDetallesProducto}
-        setModalDetallesProducto={setModalDetallesProducto}
-        addProducto={addProducto}
-        setAddProducto={setAddProducto}
-        idProducto={idProducto}
-        poderosoJson={poderosoJson}
-        register={register}
-        handleSubmit={handleSubmit}
-        errors={errors}
-        reset
-      />
-      <ToastContainer></ToastContainer>
-    </>
-  );
+			<ModificarProducto
+				modalModificarProducto={modalModificarProducto}
+				setModalModificarProducto={setModalModificarProducto}
+				addProducto={addProducto}
+				setAddProducto={setAddProducto}
+				idProducto={idProducto}
+				poderosoJson={poderosoJson}
+				register={register}
+				handleSubmit={handleSubmit}
+				errors={errors}
+				reset
+			/>
+			<DetallesProducto
+				modalDetallesProducto={modalDetallesProducto}
+				setModalDetallesProducto={setModalDetallesProducto}
+				addProducto={addProducto}
+				setAddProducto={setAddProducto}
+				idProducto={idProducto}
+				poderosoJson={poderosoJson}
+				register={register}
+				handleSubmit={handleSubmit}
+				errors={errors}
+				reset
+			/>
+			<ToastContainer></ToastContainer>
+		</>
+	);
 };
 
 export default Productos;
