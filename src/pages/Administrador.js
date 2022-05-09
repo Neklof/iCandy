bccc5900c515ab3d0283e2ea5fc993ee653bf0fb
Karl Scherzer <scherzer1999@hotmail.com>
2022-05-08 21:00:56 -0500

    Agregando botón de cerrar sesión en sesión de administrador

--- src/pages/Administrador.js
+++ src/pages/Administrador.js
@@ -12,68 +12,68 @@ import Cortecaja from "./Administrador/CorteCaja/Cortecaja";
 import Perfil from "./Cliente/Perfil/Perfil";
 
 const Administrador = ({ session, setSession }) => {
-  const userJson = window.localStorage.getItem("loggedUser");
-  const user1 = userJson ? JSON.parse(userJson) : {};
-  return (
-    <MainContainer>
-      <NavAdminContainer>
-        <Menu></Menu>
-      </NavAdminContainer>
-      <Routes>
-        <Route path="/" element={<Caja />} />
-        <Route
-          path="/productos"
-          element={
-            <AdminContainer>
-              <Productos />
-            </AdminContainer>
-          }
-        />
-        <Route
-          path="/surtir_productos"
-          element={
-            <AdminContainer>
-              <SurtirProductos />
-            </AdminContainer>
-          }
-        />
-        <Route
-          path="/usuarios"
-          element={
-            <AdminContainer>
-              <Usuarios />
-            </AdminContainer>
-          }
-        />
-        <Route
-          path="/caja"
-          element={
-            <AdminContainer>
-              <Cortecaja />
-            </AdminContainer>
-          }
-        />
-        <Route path="/historial" element={<div />} />
-        <Route
-          path="/pedidos"
-          element={
-            <AdminContainer>
-              <Pedidos />
-            </AdminContainer>
-          }
-        />
-        <Route
-          path="/configuracion"
-          element={
-            <AdminContainer>
-              <Perfil userData={user1} />
-            </AdminContainer>
-          }
-        />
-        <Route path="*" element={<div />}></Route>
-      </Routes>
-    </MainContainer>
-  );
+	const userJson = window.localStorage.getItem("loggedUser");
+	const user1 = userJson ? JSON.parse(userJson) : {};
+	return (
+		<MainContainer session={session} setSession={setSession}>
+			<NavAdminContainer>
+				<Menu></Menu>
+			</NavAdminContainer>
+			<Routes>
+				<Route path="/" element={<Caja />} />
+				<Route
+					path="/productos"
+					element={
+						<AdminContainer>
+							<Productos />
+						</AdminContainer>
+					}
+				/>
+				<Route
+					path="/surtir_productos"
+					element={
+						<AdminContainer>
+							<SurtirProductos />
+						</AdminContainer>
+					}
+				/>
+				<Route
+					path="/usuarios"
+					element={
+						<AdminContainer>
+							<Usuarios />
+						</AdminContainer>
+					}
+				/>
+				<Route
+					path="/caja"
+					element={
+						<AdminContainer>
+							<Cortecaja />
+						</AdminContainer>
+					}
+				/>
+				<Route path="/historial" element={<div />} />
+				<Route
+					path="/pedidos"
+					element={
+						<AdminContainer>
+							<Pedidos />
+						</AdminContainer>
+					}
+				/>
+				<Route
+					path="/configuracion"
+					element={
+						<AdminContainer>
+							<Perfil userData={user1} />
+						</AdminContainer>
+					}
+				/>
+				<Route path="*" element={<div />}></Route>
+			</Routes>
+		</MainContainer>
+	);
 };
 
 export default Administrador;
