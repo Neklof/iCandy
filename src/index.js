0bb89990f90019a0d095ed0e824934bcff36d97c
Victor Sandoval <distraido_99@hotmail.com>
2022-05-09 20:25:26 -0500

    Ajsute en varibles de entorno problema con mercado pago host

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-
+import endpoints from "endpoints";
+console.log(endpoints.mercadoPago);
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-	document.getElementById("root")
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
